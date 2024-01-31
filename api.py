4fe45cbaaebef715fb008f0992b4d1e0b5a14b29
augustomsantos <augustomsantos05@gmail.com>
2024-01-31 00:00:30 -0300

    Initial commit

---
+++ api.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+
+def fetch_osu_user_data(api_key, username, beatmap_id, mode):
+    # URL base da API do osu!
+    url = "https://osu.ppy.sh/api/get_user_recent"
+
+
+    # Parâmetros da API
+    params = {
+        'k': api_key,
+        'u': username,
+        'b': beatmap_id,
+        'm': mode,
+        'type': 'string'
+    }
+
+
+    # Realizar a solicitação GET à API
+    response = requests.get(url, params=params)
+
+
+    # Verificar se a solicitação foi bem-sucedida
+    if response.status_code == 200:
+        # Carregar os dados JSON da resposta
+        user_data = json.loads(response.text)
+
+
+        # Verificar se os dados do usuário estão presentes
+        if user_data:
+            return user_data
+        else:
+            return "Usuário não encontrado."
+    else:
+        return f"Erro ao conectar à API: {response.status_code}"
+
+
+# Substitua YOUR_API_KEY e YOUR_USERNAME pelos seus respectivos valores
+api_key = "07e5bae41ba15ecfb3dea2020f6c726930be3b97"
+username = "Phantom1600"
+mode = '3'
+beatmap_id = '0'
+
+
+# Buscar e imprimir dados do usuário
+user_data = fetch_osu_user_data(api_key, username, beatmap_id, mode)
+print(user_data)
+# countgeki = marvelous, #count300 = perfect, #countkatu = great, #count100 = good, #count50 = bad, #countmiss = miss
---
+++ app.py
@@ -0,0 +1,109 @@
+from flask import Flask, render_template, request
+import requests
+import json
+import math
+
+app = Flask(__name__)
+
+
+
+def fetch_osu_user_data(api_key, username, beatmap_id, mode, limit):
+    url = "https://osu.ppy.sh/api/get_user_recent"
+    params = {
+        'k': api_key,
+        'u': username,
+        'b': beatmap_id,
+        'm': mode,
+        'limit': limit,
+        'type': 'string'
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        user_data = json.loads(response.text)
+        if user_data:
+            for data in user_data:
+                data['prec'] = calcular_prec(data)
+                data['date'] = exibir_date(data)
+                beatmap_info = get_beatmap_info(data, api_key, limit)
+                data['beatmap_name'] = beatmap_info.get('title', 'Beatmap Desconhecido')
+                data['beatmap_artist'] = beatmap_info.get('artist', 'Artista desconhecido')
+                data['beatmap_difficulty'] = beatmap_info.get('version', 'Unknown')
+                if data.get('rank') == 'X':
+                    data['beatmap_rank'] = "SS"
+                else:
+                    data['beatmap_rank'] = data.get('rank', 'None')
+                    
+                data['ratio'] = calcular_ratio(data)
+                data['username_formatado'] = data.get('username', 'None')
+                
+            return user_data
+        else:
+            return "Usuário não encontrado."
+    else:
+        return f"Erro ao conectar à API: {response.status_code}"
+    
+
+
+def get_beatmap_info(data, api_key, limit):
+    url = "https://osu.ppy.sh/api/get_beatmaps"
+    beatmap_id = data.get('beatmap_id')
+    params = {
+        'k': api_key,
+        'b': beatmap_id,
+        'limit': limit,
+        'type': 'string'
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        beatmap_info = json.loads(response.text)
+        if beatmap_info:
+            return beatmap_info[0]
+    return {}  
+
+    
+def calcular_prec(data):
+    countgeki = int(data.get('countgeki', 0))
+    count300 = int(data.get('count300', 0))
+    count100 = int(data.get('count100', 0))
+    countkatu = int(data.get('countkatu', 0))
+    count50 = int(data.get('count50', 0))
+    countmiss = int(data.get('countmiss', 0))
+
+    prec = ((countgeki + count300) * 300 + countkatu * 200 + count100 * 100 + count50 * 50) / (
+            300 * (countmiss + count50 + count100 + countkatu + count300 + countgeki))*100
+    return f"{prec:.2f}%"
+
+def calcular_ratio(data):
+    countgeki = int(data.get('countgeki', 0))
+    count300 = int(data.get('count300', 0))
+
+    ratio = (countgeki/count300)
+    return f"{ratio:.2f}"
+
+def exibir_date(data):
+    date = data.get('date', 0)
+    return date
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    api_key = "07e5bae41ba15ecfb3dea2020f6c726930be3b97"
+    username = None
+    mode = '3'
+    beatmap_id = '0'
+    limit = '50'
+    user_data = []
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        
+
+        if username:
+            user_data = fetch_osu_user_data(api_key, username, beatmap_id, mode, limit)      
+
+    #beatmap_info = [data['beatmap_id'] for data in user_data]
+
+    return render_template('index.html', username=username, user_data=user_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
